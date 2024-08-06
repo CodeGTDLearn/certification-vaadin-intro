363c81d365c8a982e5e349b3470b26d10847cff3
PauloPortofolio <pvba02@gmail.com>
2024-08-06 16:24:55 -0400

    3.0) Refactoring
    * CONCLUDED:
    - Vaadin Introduction - Module 1/6 (refactoring)
    
    * WORKING:
    - Vaadin Layouting - Module 2/6
    
    * NEXT STEPS:
    - Vaadin Creating Forms - Module 3/6
    - Vaadin Datalist with Grid - Module 4/6
    - Vaadin Router API - Module 5/6
    - Vaadin Theming and Styling - Module 6/6
    
    * FUTURE FEATURES:

--- src/main/java/com/certification/introduction/views/introduction/CertTrainingView.java
+++ src/main/java/com/certification/introduction/views/introduction/CertTrainingView.java
@@ -2,12 +2,8 @@ package com.certification.introduction.views.introduction;
 
 import com.certification.introduction.views.introduction.practice.Practice_1;
 import com.certification.introduction.views.introduction.practice.Practice_2;
-import com.certification.introduction.views.introduction.practice.Practice_3;
-import com.certification.introduction.views.introduction.practice.Practice_4;
 import com.certification.introduction.views.introduction.solutions.Solution_1;
 import com.certification.introduction.views.introduction.solutions.Solution_2;
-import com.certification.introduction.views.introduction.solutions.Solution_3;
-import com.certification.introduction.views.introduction.solutions.Solution_4;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -20,8 +16,8 @@ public class CertTrainingView extends VerticalLayout {
 
   public CertTrainingView() {
 
-    //solutionSelector(1);
-    practiceSelector(1);
+    solutionSelector(2);
+//    practiceSelector(1);
   }
 
   public void solutionSelector(int solutionNumber) {
@@ -29,8 +25,6 @@ public class CertTrainingView extends VerticalLayout {
     switch (solutionNumber) {
       case 1 -> add(new Solution_1());
       case 2 -> add(new Solution_2());
-      case 3 -> add(new Solution_3());
-      case 4 -> add(new Solution_4());
       default -> throw new IllegalArgumentException("Exercise not found: " + solutionNumber);
     }
   }
@@ -40,8 +34,6 @@ public class CertTrainingView extends VerticalLayout {
     switch (practiceNumber) {
       case 1 -> add(new Practice_1());
       case 2 -> add(new Practice_2());
-      case 3 -> add(new Practice_3());
-      case 4 -> add(new Practice_4());
       default -> throw new IllegalArgumentException("Exercise not found: " + practiceNumber);
     }
   }
--- src/main/java/com/certification/introduction/views/introduction/practice/Practice_2.java
+++ src/main/java/com/certification/introduction/views/introduction/practice/Practice_2.java
@@ -1,41 +1,65 @@
 package com.certification.introduction.views.introduction.practice;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
-
-import static com.vaadin.flow.component.button.ButtonVariant.LUMO_PRIMARY;
+import com.vaadin.flow.component.upload.Upload;
 
 public class Practice_2 extends VerticalLayout {
 
   public Practice_2() {
 
-    var title = new H2("Practice Exercise 02");
+    var title = new H2("Practice Exercise 04");
     add(title);
 
-    var name = new TextField("Name:");
-    var email = new EmailField("E-mail:");
-    var birthDay = new DatePicker("BirthDate:");
-    var age = new IntegerField("Age:");
-    var address = new TextArea("Address:");
-    var comboCountry = new ComboBox<String>("Country:");
-    comboCountry.setItems("Brasil", "Portugal", "Canada", "Finland");
-    var acceptTerms = new Checkbox("I agree with the Conditions");
 
+    var eventName = new TextField("Event Name:");
+    var description = new TextArea("Event Description:");
+    var eventDate = new DatePicker("Event Date:");
+    var eventType = new ComboBox<>("Event Type:");
+    eventType.setItems("Workshop", "Webinar");
+    var qtdeAttenddes = new IntegerField();
+    var emailOrganizer = new EmailField("Organizer Email:");
     var row = new HorizontalLayout();
-    Button signUp = new Button("Sign Up");
-    signUp.addThemeVariants(LUMO_PRIMARY);
-    Button clear = new Button("Clear");
-    row.add(signUp, clear);
-    add(name, email, birthDay, age, address, comboCountry, acceptTerms, row);
+    var submitButton = new Button("Submit Registration");
+    var cancelButton = new Button("Cancel");
+    row.add(submitButton, cancelButton);
+
+    var venueType = new RadioButtonGroup<>();
+    venueType.setLabel("Venue Type:");
+    venueType.setItems("Indoor","Outdoor","Virtual");
+
+    var amenities = new CheckboxGroup<>();
+    amenities.setLabel("Amenities:");
+    amenities.setItems("Wi-fi","Catering","Parking");
+
+    Upload uploadfile = new Upload();
+    uploadfile.setAcceptedFileTypes("image/*");
+
+
+
+    //
+    add(eventName,
+        description,
+        eventDate,
+        eventType,
+        qtdeAttenddes,
+        emailOrganizer,
+        venueType,
+        amenities,
+        uploadfile,
+        row
+    );
+
 
   }
 }
